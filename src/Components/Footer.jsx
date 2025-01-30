86584ee6edfc05e92ddf70fa495c51ad40205cb1
HowlOfTheVoid <ehgraupmann@gmail.com>
2025-01-30 10:59:51 -0500

    Added Footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EventPlanner.css'; // Import CSS file for styling
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                This is part of an IBM Certification Course, completed by Ethan Graupmann.
+            </p>
+        </footer>
+    )
+}
